1354a077c76920f21ae412dc2ecde13c2b629886
apdonaghy <adonaghy@seattleu.edu>
2021-03-25 19:49:06 -0700

    revised version

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ let localDataIndex;
 let favorites = [];
 let alreadyInFavorites = false;
 let localDataItemNum;
+let catalogName = 'test';
 
-
-const storedfavorites = JSON.parse(localStorage.getItem("favorites"));
 
 const x = document.querySelector(".favoritesPanel");
 
@@ -32,9 +31,9 @@ window.onscroll = function () {
 const deleteFavorite = function (e) {
 
   for (let obj in favorites) {
-    if (e.target.dataset.index === favorites[obj]['Item Number']) {
+    if (e.target.dataset.index == favorites[obj]['Item Number']) {
       favorites.splice(favorites.indexOf(favorites[obj]), 1);
-      localStorage.setItem("favorites", JSON.stringify(favorites));
+      localStorage.setItem(`${localData[0][0]['Catalog Name']}`, JSON.stringify(favorites));
     }
   }
   console.log(favorites.length)
@@ -112,7 +111,7 @@ const addItem = function () {
 
 
   for (let obj in favorites) {
-    if (localDataItemNum === favorites[obj]['Item Number']) {
+    if (localDataItemNum == favorites[obj]['Item Number']) {
       alreadyInFavorites = true;
     }
   }
@@ -124,7 +123,7 @@ const addItem = function () {
     favorites.unshift(localData[0][localDataIndex])
 
     favorites[0].order = localDataIndex
-    localStorage.setItem("favorites", JSON.stringify(favorites));
+    localStorage.setItem(`${localData[0][0]['Catalog Name']}`, JSON.stringify(favorites));
     x.innerHTML = '';
     for (let i = 0; i < favorites.length; i++) {
       const savedItemContainer = document.createElement('div');
@@ -132,7 +131,7 @@ const addItem = function () {
       const newSavedItem = document.createElement('p');
       newSavedItem.setAttribute('data-index', favorites[i].order);
       newSavedItem.addEventListener("click", focusIn)
-      newSavedItem.innerHTML = ` -${favorites[i]['Name']} `
+      newSavedItem.innerHTML = ` -${favorites[i]['Product Name']} `
       const deleteBtn = document.createElement('img')
       deleteBtn.setAttribute('data-index', favorites[i]['Item Number']);
       deleteBtn.addEventListener('click', deleteFavorite)
@@ -200,7 +199,7 @@ const focusIn = function (e) {
   }
 
   const itemTitle = document.createElement('h4');
-  itemTitle.innerHTML = localObject['Name']
+  itemTitle.innerHTML = localObject['Product Name']
   productInfo.appendChild(itemTitle)
 
   const itemNumAddContainer = document.createElement('div');
@@ -218,7 +217,7 @@ const focusIn = function (e) {
 
   const description = document.createElement('p');
   description.setAttribute('class', 'description')
-  description.innerHTML = localObject['Item Description']
+  description.innerHTML = localObject['COPY']
   productInfo.appendChild(description)
 
   const packaging = document.createElement('p');
@@ -227,9 +226,9 @@ const focusIn = function (e) {
   productInfo.appendChild(packaging)
 
   const pricing2 = document.createElement('p');
-  pricing2.setAttribute('class', 'pricing2')
-  pricing2.innerHTML = localObject['Pricing']
-  productInfo.appendChild(pricing2)
+  pricing2.setAttribute('class', 'pricing2');
+  pricing2.innerHTML = `${localObject['Price Each']} ea. / ${localObject['SELL PACK']} pack (${localObject['SELL UNIT']} pc.)`;
+  productInfo.appendChild(pricing2);
 
 
   // const focusContent = e.target.parentElement.innerHTML;
@@ -266,14 +265,55 @@ clipboard.addEventListener("copy", function (event) {
 
 $(document).ready(function () {
 
-  $.getJSON("opportunity_buy.json", function (data) {
+  $.getJSON("csvjson.json", function (data) {
+
     const headline = document.getElementById('catalogName')
 
     headline.innerHTML = data[0]['Catalog Name'];
 
-    for (let key in data) {
+    const header = document.querySelector('.header')
+    const line = document.querySelector('.line')
+    const footerColor = document.querySelector('.footer')
+    const catalogName = document.querySelector('#catalogName')
+    const copyright = document.querySelector('.copyright')
+    if(data[0]['Color Scheme'] === 'Blue'){
+      header.style.backgroundImage = "linear-gradient(to right, #2aafec, #3b70ed)";
+      line.style.backgroundImage = "linear-gradient(to right, #2aafec, #3b70ed)";
+      footerColor.style.backgroundImage = "linear-gradient(to right, rgb(9, 15, 28), #0d2b4b)";
+      catalogName.style.color = "#3b70ed";
+    }
 
+    if(data[0]['Color Scheme'] === 'Purple'){
+      header.style.backgroundImage = "linear-gradient(to right, #5b00bb, #5d04ff)";
+      line.style.backgroundImage = "linear-gradient(to right,#5b00bb, #5d04ff)";
+      footerColor.style.backgroundImage = "linear-gradient(to right, #1a001e, #391676)";
+      catalogName.style.color = "#6521ff";
+      getComputedStyle(document.documentElement).getPropertyValue('--hover');
+      document.documentElement.style.setProperty('--hover', '#6521ff');
+    }
+
+    if(data[0]['Color Scheme'] === 'Orange'){
+      header.style.backgroundImage = "linear-gradient(to right, #ff6200, #ff8506)";
+      line.style.backgroundImage = "linear-gradient(to right, #ff6200, #ff8506)";
+      footerColor.style.backgroundImage = "linear-gradient(to right, black, black)";
+      catalogName.style.color = "#e74f00";
+      getComputedStyle(document.documentElement).getPropertyValue('--hover');
+      document.documentElement.style.setProperty('--hover', '#ff8506');
+    }
+
+    if(data[0]['Color Scheme'] === 'Green'){
+      header.style.backgroundImage = "linear-gradient(to right, #008538, #56d72d)";
+      line.style.backgroundImage = "linear-gradient(to right, #008538 , #56d72d)";
+      footerColor.style.backgroundImage = "linear-gradient(to right, #000f00, #266814)";
+      catalogName.style.color = "#008538";
+      getComputedStyle(document.documentElement).getPropertyValue('--hover');
+      document.documentElement.style.setProperty('--hover', '#31b424');
+    }
+
+    for (let key in data) {
+    
       const item = data[key];
+      if(item['imageURL1'] !== ''){
       const itemContainer = document.createElement('div');
       itemContainer.setAttribute('class', 'itemContainer')
       container.appendChild(itemContainer)
@@ -294,7 +334,8 @@ $(document).ready(function () {
       }
 
       const title = document.createElement('h3');
-      title.innerHTML = item['Name'];
+      title.setAttribute('class', 'titleHover')
+      title.innerHTML = item['Product Name'];
       title.setAttribute('data-index', data.indexOf(item));
       title.addEventListener("click", focusIn)
       itemContainer.appendChild(title);
@@ -304,13 +345,13 @@ $(document).ready(function () {
       //  itemContainer.appendChild(itemNum);
 
       const price = document.createElement('p');
-      price.innerHTML = `${item['Pricing']}`;
+      price.innerHTML = `${item['Price Each']} ea. / ${item['SELL PACK']} pack (${item['SELL UNIT']} pc.)` ;
       price.setAttribute('data-index', data.indexOf(item));
       price.addEventListener("click", focusIn)
       itemContainer.appendChild(price);
       item.order = data.indexOf(item)
     }
-
+  }
     //   for(let key of data){
 
 
@@ -347,26 +388,26 @@ $(document).ready(function () {
     localData.push(data);
     console.log(localData)
 
-    const storedfavorites = JSON.parse(localStorage.getItem("favorites"));
+    const storedfavorites = JSON.parse(localStorage.getItem(`${data[0]['Catalog Name']}`));
     if (storedfavorites && storedfavorites.length >= 1) {
   
       
       x.innerHTML = '';
       for (const individualLocalData in localData[0]) {
         for(let item in storedfavorites){
-          if(storedfavorites[item]['Item Number'] === localData[0][individualLocalData]['Item Number']){
+          if(storedfavorites[item]['Item Number'] == localData[0][individualLocalData]['Item Number']){
             favorites.push(storedfavorites[item])
           }
         }
         for (let i = 0; i < favorites.length; i++) {
-          if (favorites[i]['Item Number'] === localData[0][individualLocalData]['Item Number']) {
+          if (favorites[i]['Item Number'] == localData[0][individualLocalData]['Item Number']) {
             const savedItemContainer = document.createElement('div')
             savedItemContainer.setAttribute('class', 'savedItemContainer');
             const newSavedItem = document.createElement('p');
             console.log(localData[0].indexOf(localData[0][individualLocalData]))
             newSavedItem.setAttribute('data-index', localData[0].indexOf(localData[0][individualLocalData]));
             newSavedItem.addEventListener("click", focusIn)
-            newSavedItem.innerHTML = ` -${favorites[i]['Name']} `
+            newSavedItem.innerHTML = ` -${favorites[i]['Product Name']} `
             const deleteBtn = document.createElement('img')
             deleteBtn.setAttribute('data-index', favorites[i]['Item Number']);
             deleteBtn.addEventListener('click', deleteFavorite)
